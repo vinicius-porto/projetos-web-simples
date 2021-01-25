1d06b20568386ddf965a26451f246b3ca7f846bd
Vinicius Porto <55770422+vinicius-porto@users.noreply.github.com>
2021-01-25 13:48:46 -0300

    Add files via upload

---
+++ Açai/js/action.js
@@ -0,0 +1,7 @@
+function message(){
+    var msg =  alert('Pedido em andamento aguarde.....');
+    var msg2 = alert('Concluido com sucesso');
+    var msg3 = alert('Agradecemos seu pedido volte sempre');
+    
+    
+}
